dbde73fae4053f99f8493663976af78566a1e2bc
Paul Shahid <pshahid90@gmail.com>
2017-01-18 23:36:19 -0500

    Adding models, db loading

--- walkit/web/models.py
+++ walkit/web/models.py
@@ -1,1 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Subreddit(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+    created = db.Column(db.Date())
+    subscribers = db.Column(db.Integer)
--- walkit/web/walkit.py
+++ walkit/web/walkit.py
@@ -1,6 +1,9 @@
-from flask import Flask
+from flask import Flask, g
+
+from .models import db
 
 app = Flask(__name__)
+db.init_app(app)
 
 @app.route("/")
 def index():
